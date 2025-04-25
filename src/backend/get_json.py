37e64f111484a83569672354bd7d87df3cecdd9a
Daniyal <daniyalshagiroff@gmail.com>
2025-04-25 23:45:11 +0400

    Add check ups for backend

--- src/backend/get_json.py
+++ src/backend/get_json.py
@@ -11,15 +11,13 @@ data = {
     "income_annum": None,
     "loan_amount": None,
     "loan_term": 4,
-    "cibil_score": 500,
+    "cibil_score": 400,
     "residential_assets_value": None,
     "commercial_assets_value": 2200000,
     "luxury_assets_value": 8800000,
     "bank_asset_value": None
 }
 
-# Преобразование в JSON и отправка POST-запроса
 res = requests.post(url, json=data)
 
-# Вывод результата
 print(res.json())
